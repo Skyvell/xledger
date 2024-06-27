005b9510f5eb7bfcbef2aa2c236a92456aadac9d
Ted Skyvell <tskyvell@gmail.com>
2024-06-27 12:16:39 +0200

    Get creds.

--- functions/timesheets/syncronize_timesheets.py
+++ functions/timesheets/syncronize_timesheets.py
@@ -1,4 +1,5 @@
 from azure import functions as func
+from azure.identity import DefaultAzureCredential
 import logging
 import os
 
@@ -26,6 +27,10 @@ bp = func.Blueprint()
 @bp.schedule(schedule="0 0 * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def syncronize_timesheets(myTimer: func.TimerRequest) -> None:
+    credential = DefaultAzureCredential()
+
+
+
     logging.info('Trigger SyncronizeTimesheets.')
     ## Get environment variables.
     #api_endpoint = os.getenv("Endpoint")
