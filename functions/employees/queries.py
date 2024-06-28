8de0bdff421900bdc7642151661a3dd8b63d9c2f
Ted Skyvell <tskyvell@gmail.com>
2024-06-28 17:17:29 +0200

    Factored oout Node fields from queries. Derive parquet columns from the node fields directly.

--- functions/employees/queries.py
+++ functions/employees/queries.py
@@ -1,147 +1,114 @@
 from gql import gql
+from shared.utils.data_transformation import flatten_graphql_fields
 
 
-GET_EMPLOYEES_FROM_DBIDS = gql("""
-    query getEmployees($first: Int, $after: String, $dbIdList: [Int64String!]) {
+# Define all the fields that we want to fetch from the xledger API here. 
+# This way we only need to add/remove fields in one place.
+EMPLOYEE_NODE_FIELDS = """
+    dbId
+    email
+    description
+    createdAt
+    modifiedAt
+    employmentFrom
+    employmentTo
+    positionValue {
+        dbId
+        description
+        code
+    }
+    positionCategory {
+        dbId
+        description
+        code
+    }
+    compensationType {
+        dbId
+        description
+        code
+    }
+    employmentType {
+        description
+        owner {
+            description
+        }
+    }
+    contact {
+        firstName
+        lastName
+        birthday
+        age
+        country {
+            description
+        }
+        gender {
+            name
+        }
+    }
+    exitReason {
+        dbId
+        description
+        code
+    }
+    glObject1 {
+        dbId
+        description
+        code
+        id
+    }
+"""
+
+
+# This is the final list of columns that we want in the pandas dataframe,
+# and the resulting parquet file.
+# Derived directly from the EMPLOYEE_NODE_FIELDS above to make sure the columns
+# Are deterministic and up-to date.
+COLUMNS = flatten_graphql_fields(EMPLOYEE_NODE_FIELDS)
+
+
+# Query to get employees from a list of database IDs
+GET_EMPLOYEES_FROM_DBIDS = gql(f"""
+    query getEmployees($first: Int, $after: String, $dbIdList: [Int64String!]) {{
         employees(
             first: $first,
             after: $after, 
-            filter: { 
+            filter: {{ 
                 dbId_in: $dbIdList
-            }
-        ) {
-            edges {
+            }}
+        ) {{
+            edges {{
                 cursor
-                node {
-                    dbId
-                    email
-                    description
-                    createdAt
-                    modifiedAt
-                    employmentFrom
-                    employmentTo
-                    positionValue {
-                        dbId
-                        description
-                        code
-                    }
-                    positionCategory {
-                        dbId
-                        description
-                        code
-                    }
-                    compensationType {
-                        dbId
-                        description
-                        code
-                    }
-                    employmentType {
-                        description
-                        owner {
-                            description
-                        }
-                    }
-                    contact {
-                        firstName
-                        lastName
-                        birthday
-                        age
-                        country {
-                            description
-                        }
-                        gender {
-                            name
-                        }
-                    }
-                    exitReason {
-                        dbId
-                        description
-                        code
-                    }
-                    glObject1 {
-                        dbId
-                        description
-                        code
-                        id
-                    }
-                }
-            }
-            pageInfo {
+                node {{
+                    {EMPLOYEE_NODE_FIELDS}
+                }}
+            }}
+            pageInfo {{
                 hasNextPage
-            }
-        }
-    }
+            }}
+        }}
+    }}
 """)
 
-GET_EMPLOYEES_AFTER_CURSOR = gql("""
-    query getEmployees($first: Int, $last: Int, $after: String) {
+GET_EMPLOYEES_AFTER_CURSOR = gql(f"""
+    query getEmployees($first: Int, $last: Int, $after: String) {{
         employees(
             first: $first,
             last: $last,
             after: $after
-        ) {
-            edges {
+        ) {{
+            edges {{
                 cursor
-                node {
-                    dbId
-                    email
-                    description
-                    createdAt
-                    modifiedAt
-                    employmentFrom
-                    employmentTo
-                    positionValue {
-                        dbId
-                        description
-                        code
-                    }
-                    positionCategory {
-                        dbId
-                        description
-                        code
-                    }
-                    compensationType {
-                        dbId
-                        description
-                        code
-                    }
-                    employmentType {
-                        description
-                        owner {
-                            description
-                        }
-                    }
-                    contact {
-                        firstName
-                        lastName
-                        birthday
-                        age
-                        country {
-                            description
-                        }
-                        gender {
-                            name
-                        }
-                    }
-                    exitReason {
-                        dbId
-                        description
-                        code
-                    }
-                    glObject1 {
-                        dbId
-                        description
-                        code
-                        id
-                    }
-                }
-            }
-            pageInfo {
+                node {{
+                    {EMPLOYEE_NODE_FIELDS}
+                }}
+            }}
+            pageInfo {{
                 hasNextPage
-            }
-        }
-    }
+            }}
+        }}
+    }}
 """)
+
 
 GET_EMPLOYEE_DELTAS = gql("""
     query getEmployeeDeltas($first: Int, $last: Int, $after: String) {
