3cd39406d6356d48f48d1e5a33387e5c123ec1f2
Ted Skyvell <tskyvell@gmail.com>
2024-07-01 17:23:54 +0200

    Add projects support.

--- functions/projects/queries.py
+++ functions/projects/queries.py
@@ -6,48 +6,20 @@ from shared.utils.data_transformation import flatten_graphql_fields
 # This way we only need to add/remove fields in one place.
 PROJECT_NODE_FIELDS = """
     dbId
-    createdAt
-    modifiedAt
-    assignmentDate
-    workingHours
-    isHeaderApproved
-    headerApprovedAt
-    hourlyRevenueCurrency
-    owner {
-        dbId
-        description
-    }
-    employee {
-        dbId
-        description
-        code
-    }
-    activity {
-        code
-        description
-    }
-    timeType {
-        code
-        description
-    }
-    project {
-        dbId
-        description
-        code
-    }
+    description
 """
 
 
 # This is the final list of columns that we want in the pandas dataframe,
 # and the resulting parquet file.
-# Derived directly from the TIMESHEET_NODE_FIELDS above to make sure the columns
+# Derived directly from the PROJECT_NODE_FIELDS above to make sure the columns
 # Are deterministic and up-to date.
-COLUMNS = flatten_graphql_fields(TIMESHEET_NODE_FIELDS)
+COLUMNS = flatten_graphql_fields(PROJECT_NODE_FIELDS)
 
 
-GET_TIMESHEETS_FROM_DBIDS = gql(f"""
-    query getTimesheets($first: Int, $after: String, $dbIdList: [Int64String!]) {{
-        timesheets(
+GET_PROJECTS_FROM_DBIDS = gql(f"""
+    query getProjects($first: Int, $after: String, $dbIdList: [Int64String!]) {{
+        projects(
             first: $first,
             after: $after, 
             filter: {{ 
@@ -56,7 +28,7 @@ GET_TIMESHEETS_FROM_DBIDS = gql(f"""
         ) {{
             edges {{
                 node {{
-                    {TIMESHEET_NODE_FIELDS}
+                    {PROJECT_NODE_FIELDS}
                 }}
                 cursor
             }}
@@ -68,15 +40,15 @@ GET_TIMESHEETS_FROM_DBIDS = gql(f"""
 """)
 
 
-GET_TIMESHEETS_AFTER_CURSOR = gql(f"""
-    query getTimesheets($first: Int, $after: String) {{
-        timesheets(
+GET_PROJECTS_AFTER_CURSOR = gql(f"""
+    query getProjects($first: Int, $after: String) {{
+        projects(
             first: $first,
             after: $after
         ) {{
             edges {{
                 node {{
-                    {TIMESHEET_NODE_FIELDS}
+                    {PROJECT_NODE_FIELDS}
                 }}
                 cursor
             }}
@@ -88,9 +60,9 @@ GET_TIMESHEETS_AFTER_CURSOR = gql(f"""
 """)
 
 
-GET_TIMESHEET_DELTAS = gql("""
-    query getTimesheetDeltas($first: Int, $last: Int, $after: String) {
-        timesheet_deltas(
+GET_PROJECT_DELTAS = gql("""
+    query getProjectDeltas($first: Int, $last: Int, $after: String) {
+        project_deltas(
             first: $first,
             last: $last, 
             after: $after
