9282152d742fe0b88b1b099224e1e20e882dc3aa
Ted Skyvell <tskyvell@gmail.com>
2024-07-02 16:00:55 +0200

    Register new functions.

--- function_app.py
+++ function_app.py
@@ -5,6 +5,8 @@ from functions.customers.syncronize_customers import bp as customers_bp
 from functions.employees.syncronize_employees import bp as employees_bp
 from functions.projects.syncronize_projects import bp as projects_bp
 from functions.suppliers.syncronize_suppliers import bp as suppliers_bp
+from functions.ap_transactions.syncronize_ap_transactions import bp as ap_transactions_bp
+from functions.ar_transactions.syncronize_ar_transactions import bp as ar_transactions_bp
 
 
 # Create the function app.
@@ -16,3 +18,5 @@ app.register_blueprint(customers_bp)
 app.register_blueprint(employees_bp)
 app.register_blueprint(projects_bp)
 app.register_blueprint(suppliers_bp)
+app.register_blueprint(ap_transactions_bp)
+app.register_blueprint(ar_transactions_bp)
