d717c54530ae905c91991107e44332113a4c64b4
Ted Skyvell <tskyvell@gmail.com>
2024-07-03 10:59:03 +0200

    New provider

--- infrastructure/dev/provider.tf
+++ infrastructure/dev/provider.tf
@@ -1,5 +1,27 @@
-provider "azurerm" {
-  features {}
-  # This tells Terraform to use the credentials from the Azure CLI login
-  use_cli = true
+terraform {
+  required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">=0.1.0"
+    }
+  }
 }
+
+provider "azuredevops" {
+  org_service_url = var.org_service_url
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
+  client_secret   = var.client_secret
+}
+
+
+
+
+
+
+
+#provider "azurerm" {
+#  features {}
+#  # This tells Terraform to use the credentials from the Azure CLI login
+#  use_cli = true
+#}
