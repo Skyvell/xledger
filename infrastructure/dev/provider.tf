253c0bc582124b06764ecec137f723aa15cdcc0c
Ted Skyvell <tskyvell@gmail.com>
2024-07-03 14:19:24 +0200

    Test again.

--- infrastructure/dev/provider.tf
+++ infrastructure/dev/provider.tf
@@ -1,24 +1,5 @@
-terraform {
-  required_providers {
-    azuredevops = {
-      source  = "microsoft/azuredevops"
-      version = ">=0.1.0"
-    }
-  }
+provider "azurerm" {
+  features {}
+  # This tells Terraform to use the credentials from the Azure CLI login
+  # use_cli = true
 }
-
-provider "azuredevops" {
-  org_service_url = var.org_service_url
-}
-
-
-
-
-
-
-
-#provider "azurerm" {
-#  features {}
-#  # This tells Terraform to use the credentials from the Azure CLI login
-#  use_cli = true
-#}
