af83e50648671dde5b9f262c210682ee083d6fb7
Ted Skyvell <tskyvell@gmail.com>
2024-07-11 23:09:50 +0200

    Add changes.

--- function_app.py
+++ function_app.py
@@ -1,11 +1,11 @@
 
 from azure import functions as func
 from functions.timesheets.syncronize import bp as timesheets_bp
-from functions.customers.syncronize_customers import bp as customers_bp
+from functions.customers.syncronize import bp as customers_bp
 from functions.employees.syncronize import bp as employees_bp
 from functions.projects.syncronize import bp as projects_bp
 from functions.suppliers.syncronize import bp as suppliers_bp
-from functions.ap_transactions.syncronize_ap_transactions import bp as ap_transactions_bp
+from functions.ap_transactions.syncronize import bp as ap_transactions_bp
 from functions.ar_transactions.syncronize import bp as ar_transactions_bp
 from functions.transactions.syncronize import bp as transactions_bp
 
