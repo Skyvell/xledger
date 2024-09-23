1e684935f6cd802177561294b2d0ba8ce160a39b
Ted Skyvell <tskyvell@gmail.com>
2024-09-23 21:47:57 +0200

    Fix bug.

--- shared/utils/data_transformation.py
+++ shared/utils/data_transformation.py
@@ -142,9 +142,10 @@ def flatten_graphql_fields(input: str, separator: str = ".") -> List[str]:
 
 def infer_pandas_dataframe_schema(df: pd.DataFrame) -> dict:
     """
-    Infers the schema from a DataFrame, returning a dictionary of column names and their datatypes.
+    Infers the schema from a DataFrame, returning a dictionary of column names and their datatypes
+    as strings, so that it can be serialized with json.dumps().
     """
-    schema = {col: df[col].dtype for col in df.columns}
+    schema = {col: str(df[col].dtype) for col in df.columns}
     return schema
 
 
