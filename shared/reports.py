25d04be0fa19760513bf5c87e7c3668a23d7e2f9
Ted Skyvell <tskyvell@gmail.com>
2024-09-24 12:53:41 +0200

    Revert "Add new report function."
    This reverts commit 2c559f7ef1f802ad52321ab8fcf1198fbc3c8d0d.

--- shared/reports.py
+++
@@ -1,8 +0,0 @@
-import requests
-from typing import Union, Dict, List, Any
-
-
-def get_report(url: str, params: dict = None) -> Union[Dict[str, Any], List[Any]]:
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    return response.json()
