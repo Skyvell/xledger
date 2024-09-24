a311e848022df133a0756c92f38114556151d11e
Ted Skyvell <tskyvell@gmail.com>
2024-09-24 12:57:33 +0200

    Now all columns have datatype object.

--- shared/utils/files.py
+++ shared/utils/files.py
@@ -65,7 +65,7 @@ def convert_dicts_to_parquet_pandas(data: list[dict], columns: list[str]) -> io.
     io.BytesIO: A BytesIO buffer containing the Parquet file.
     """
     # Create a DataFrame from the list of dictionaries, including only specified columns.
-    df = pd.DataFrame(data, columns=columns)
+    df = pd.DataFrame(data, columns=columns, dtype=object)
     
     # Create a BytesIO buffer to hold the Parquet data.
     buffer = io.BytesIO()
