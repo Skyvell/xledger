cfb9f3f22bb484b69938b95621d9ac736e27bea8
Ted Skyvell <tskyvell@gmail.com>
2024-10-01 10:08:36 +0200

    Reformat code.

--- functions/cost_categories/get_cost_categories.py
+++ functions/cost_categories/get_cost_categories.py
@@ -1,6 +1,6 @@
-from azure import functions as func
-from azure.identity import DefaultAzureCredential
 import logging
+import azure.functions as func
+from azure.identity import DefaultAzureCredential
 
 from shared.data_lake_writer import DataLakeWriter
 from shared.environment_config import EnvironmentConfig
@@ -18,8 +18,12 @@ logging.basicConfig(level=logging.INFO)
 bp = func.Blueprint()
 
 @bp.function_name(f"get_{NAME}")
-@bp.schedule(schedule="0 0 0 * * *", arg_name="myTimer", run_on_startup=False,
-              use_monitor=False) 
+@bp.schedule(
+    schedule="0 0 0 * * *", 
+    arg_name="myTimer", 
+    run_on_startup=False,
+    use_monitor=False
+)
 def report(myTimer: func.TimerRequest) -> None:
     # Get credentials.
     credential = DefaultAzureCredential()
@@ -28,7 +32,12 @@ def report(myTimer: func.TimerRequest) -> None:
     config = EnvironmentConfig()
 
     # Initialize writer.
-    data_lake_writer = DataLakeWriter(config.data_storage_account, credential, config.data_storage_container, NAME)
+    data_lake_writer = DataLakeWriter(
+        config.data_storage_account, 
+        credential, 
+        config.data_storage_container, 
+        NAME
+    )
 
     # Initialize FlexLinkReader.
     flex_link_reader = FlexLinkReader()
