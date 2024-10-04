9b9824725194bdabf56594c473c4245f88d87080
Ted Skyvell <tskyvell@gmail.com>
2024-10-04 09:59:22 +0200

    Change output dir

--- functions/cost_categories/get_cost_categories.py
+++ functions/cost_categories/get_cost_categories.py
@@ -36,7 +36,7 @@ def report(myTimer: func.TimerRequest) -> None:
     data_lake_writer = DataLakeWriter(
         config.data_storage_account, 
         credential, 
-        config.data_storage_container, 
+        OUTPUT_DIR, 
         NAME
     )
 
@@ -45,4 +45,4 @@ def report(myTimer: func.TimerRequest) -> None:
     
     # Read data from flexlink and write to blob storage.
     data = flex_link_reader.read_xlsx_flex_link(FLEX_LINK, COLUMN_DTYPES)
-    data_lake_writer.write_data(f"{get_current_time_for_filename()}-{NAME}.parquet", data, directory_name=OUTPUT_DIR)
+    data_lake_writer.write_data(f"{get_current_time_for_filename()}-{NAME}.parquet", data)
