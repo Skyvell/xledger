981654b68c719bfc894ec5c8399c74359d6a9096
Ted Skyvell <tskyvell@gmail.com>
2024-10-29 16:41:22 +0100

    Add all columns pricelist

--- functions/price_list/settings.py
+++ functions/price_list/settings.py
@@ -1,14 +1,16 @@
 # Flexlink pointing to the Excel file containing the cost categories.
-FLEX_LINK = "https://demo.xledger.net/Flex/112706068539122.xlsx?t=Ag9kyQ8yb9hkFr9HxgcatsLmIFrxZAiRUMEIYXnHnSozlTE3dzUvbnKa4ab33B0exDIND9OIoXYHzvgjXWfms1HJL_Nc-kJIvaqoKe6AEINnWmESBU9bKUnJiDG7NP4Zql_mrAK2en-Xe_yN9au2xkKklMfFu3IyRUGJWgeGSNJSgwicHAvVzRMlNi1yVaXCuAqSxhu4o5xbOs9xLmYUBfcjvzI27cNDoBpnUvwRB4qDiC02Kw6_coFm6jUoaG9nlnLez9lwOlbXjyCdBUbOOLBNxPutvhiIQG2x7yCIGe8l3ujVCPqZQ5RSERe62sxRRUaawdR5rPNEEcLaVoCs"
+FLEX_LINK = "https://demo.xledger.net/Flex/112706068539122.xlsx?t=Ag9kycMmAtgCg3WGLB3FZWp0cW4Kqu1itaKyv1qeU86SKgrN5kUiIjYxY6KwtkaWcJGB3XqI3vWCAt1nqhKR6x9vrHLL6rHIlnEZUr2I0q5ZnKT44B_hM0yAiDv8EYe5g2dY0IFIbOe6Qdd8HCERYO8C3mbQo_U_nVNlDvrRpa_by_A5D8FwPUGd37JSZOIHR6RSXWHCJ_9f_ihMCLEniGHrQwJzzm1jlhABX5hlj_0Tifx-Nfq_8aNFWHYR8G4MMfERRber5WPVyZooW88DtLI4yD2eKRzyg1owGiHI6c4SEOPpVJrdWAiPjuvMXca_4PatqNnT3E2KU9B9xwtz"
 
 # Only include these columns in the Parquet file.
 COLUMNS = [
     "Pricelist",
     "Product",
+    "Product Group",
     "Project",
     "Object",
     "Object Value",
     "Date From",
+    "Date To",
     "Unit",
     "Cur",
     "Price",
@@ -16,6 +18,8 @@ COLUMNS = [
 
 # Define the pandas data types for each column.
 COLUMN_DATA_TYPES = [
+    "string",
+    "string",
     "string",
     "string",
     "string",
