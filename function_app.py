7e0f8c9b39589a4d3c6c1155327b177565073705
Ted Skyvell <tskyvell@gmail.com>
2024-10-31 09:44:23 +0100

    Added flexlink functions.

--- function_app.py
+++ function_app.py
@@ -21,7 +21,9 @@ from functions.price_list.get_price_list import bp as price_list_bp
 # Create the function app.
 app = func.FunctionApp()
 
-# Register all the functions here for the app.
+# Register all the functions below here for the app.
+
+# API Syncronization functions.
 app.register_blueprint(timesheets_bp)
 app.register_blueprint(customers_bp)
 app.register_blueprint(employees_bp)
@@ -30,12 +32,16 @@ app.register_blueprint(suppliers_bp)
 app.register_blueprint(ap_transactions_bp)
 app.register_blueprint(ar_transactions_bp)
 app.register_blueprint(transactions_bp)
+
+# Cleanup functions.
 app.register_blueprint(reset_state_bp)
 app.register_blueprint(wipe_storage_bp)
 app.register_blueprint(reset_state_and_wipe_storage_bp)
-# app.register_blueprint(report_bp)
-# app.register_blueprint(cost_categories_bp)
-# app.register_blueprint(project_cost_setups_bp)
-# app.register_blueprint(cost_element_per_time_type_bp)
+
+# Flexlink functions.
+app.register_blueprint(report_bp)
+app.register_blueprint(cost_categories_bp)
+app.register_blueprint(project_cost_setups_bp)
+app.register_blueprint(cost_element_per_time_type_bp)
 app.register_blueprint(project_managers_bp)
 app.register_blueprint(price_list_bp)
