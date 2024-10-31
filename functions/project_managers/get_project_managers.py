11aa2cabc4387cd5facd37e1e986bb9c8725db20
Ted Skyvell <tskyvell@gmail.com>
2024-10-31 14:06:16 +0100

    Remove timestamp project managers.

--- functions/project_managers/get_project_managers.py
+++ functions/project_managers/get_project_managers.py
@@ -45,4 +45,4 @@ def report(myTimer: func.TimerRequest) -> None:
     
     # Read data from flexlink and write to blob storage.
     data = flex_link_reader.read_xlsx_flex_link(FLEX_LINK, COLUMN_DTYPES)
-    data_lake_writer.write_data(f"{get_current_time_for_filename()}-{NAME}.parquet", data)
+    data_lake_writer.write_data(f"{NAME}.parquet", data)
