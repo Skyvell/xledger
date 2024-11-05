d41cdeae01e6ba3f95b5263aeee0ce4cbd08b4a9
Ted Skyvell <tskyvell@gmail.com>
2024-11-05 12:32:09 +0100

    Format.

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -71,8 +71,8 @@ resource "azurerm_linux_function_app" "function_app" {
     "DATA_STORAGE_CONTAINER_NAME" = var.data_storage_container_name,
     "APP_CONFIG_ENDPOINT"         = azurerm_app_configuration.app_configuration.endpoint,
     "COST_CATEGORIES_FLEX_LINK"   = var.cost_categories_flex_link,
-    "EMPLOYEE_GROUPS_FLEX_LINK"  = var.employee_groups_flex_link,
-    "EMPLOYMENT_TYPES_FLEX_LINK" = var.employment_types_flex_link
+    "EMPLOYEE_GROUPS_FLEX_LINK"   = var.employee_groups_flex_link,
+    "EMPLOYMENT_TYPES_FLEX_LINK"  = var.employment_types_flex_link
   }
 }
 
