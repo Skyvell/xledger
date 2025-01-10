41f50d7abaaaf351143167bd6ec99415ee1dc63e
Ted Skyvell <tskyvell@gmail.com>
2025-01-10 09:46:09 +0100

    Added site columns. flexifields..

--- functions/api_functions/employees/queries.py
+++ functions/api_functions/employees/queries.py
@@ -74,6 +74,12 @@ NODE_FIELDS = """
     superior {
         description
     }
+    flexiFieldsItem {
+        code1 {
+            description
+            code
+        }
+    }
 """
 
 # Datatypes of the columns in the resulting pandas dataframe.
@@ -115,7 +121,9 @@ COLUMN_DATA_TYPES = [
     'string',           # glObject1.description
     'string',           # glObject1.code
     'string',           # glObject1.id
-    'string'            # superior.description
+    'string',            # superior.description
+    'string',           # flexiFieldsItem.code1.description
+    'string',           # flexiFieldsItem.code1.code
 ]
 
 
