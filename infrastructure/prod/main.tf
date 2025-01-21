f515ba5b569384943a47fd1dcb879f5d98c5dfc8
Ted Skyvell <tskyvell@gmail.com>
2025-01-21 11:12:22 +0100

    Removed reserved.

--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -33,7 +33,6 @@ resource "azurerm_service_plan" "service_plan" {
   resource_group_name = data.azurerm_resource_group.existing.name
   os_type             = "Linux"
   sku_name            = "B1"
-  reserved            = true
 }
 
 resource "azurerm_application_insights" "application_insights" {
