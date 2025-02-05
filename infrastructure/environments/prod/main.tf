109a6ab4cb0efb041becb733147a25d7bb0ec098
Ted Skyvell <tskyvell@gmail.com>
2025-02-05 11:36:29 +0100

    Fix prod variables.

--- infrastructure/environments/prod/main.tf
+++ infrastructure/environments/prod/main.tf
@@ -10,13 +10,13 @@ module "shared_data" {
 module "function_app" {
   source                              = "../../modules/function-app"
   app_configuration_name              = "xledger-syncronizer-state-prod"
-  function_app_storage_account_name   = "syncronizerstorageprod"
+  app_storage_account_name            = "syncronizerstorageprod"
   location                            = "westeurope"
   app_service_plan_name               = "xledeger-syncronizer-asp-prod"
   app_insights_name                   = "xledger-syncronizer-ai-prod"
   function_app_name                   = "xledger-syncronizer-prod"
   app_config_name                     = "xledger-syncronizer-statemanager-prod"
-  api_endpoint                        = "https://demo.xledger.net/graphql"
+  api_endpoint                        = "https://www.xledger.net/graphql"
   api_key                             = var.api_key
 
   # Flexlinks.
@@ -26,7 +26,7 @@ module "function_app" {
   financial_results_flex_link         = var.financial_results_flex_link
 
   # Existing resources.
-  data_storage_account_name           = module.shared_data.data_storage_account_name
-  data_storage_container_name         = module.shared_data.data_storage_container_name
-  resource_group_name                 = module.shared_data.app_resource_group
+  app_data_storage_account            = module.shared_data.app_storage_account
+  app_data_storage_container          = module.shared_data.app_storage_container
+  app_resource_group                  = module.shared_data.app_resource_group
 }
