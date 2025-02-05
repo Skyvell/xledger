92484f92ecb6fbc21f87a6af284edde092d543f0
Ted Skyvell <tskyvell@gmail.com>
2025-02-05 11:33:55 +0100

    Dont use cli

--- infrastructure/environments/dev/provider.tf
+++ infrastructure/environments/dev/provider.tf
@@ -3,6 +3,6 @@ provider "azurerm" {
 
   # This tells Terraform to use the credentials from the Azure CLI login.
   # Uncomment when run from command line.
-  use_cli = true
-  subscription_id = "a42468e7-3510-409f-849a-111d4574481d"
+  # use_cli = true
+  # subscription_id = "a42468e7-3510-409f-849a-111d4574481d"
 }
