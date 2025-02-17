8d478acb4ae9bd173a8b47cecb2c9cd97fe2795b
Ted Skyvell <tskyvell@gmail.com>
2025-02-17 11:27:04 +0100

    Convert string to int.

--- functions/cleanup_functions/full_data_resync/settings.py
+++ functions/cleanup_functions/full_data_resync/settings.py
@@ -6,7 +6,7 @@ from shared.utils.time import (
 # Start and end period to fectch financial results from.
 # The default end-period is current_month - 1.
 FINANCIAL_RESULTS_START_PERIOD = 2401
-FINANCIAL_RESULTS_END_PERIOD = get_previous_month_yy_mm()
+FINANCIAL_RESULTS_END_PERIOD = int(get_previous_month_yy_mm())
 
 # Generated periods based on start and end period.
-FINANCIAL_RESULTS_PERIODS = generate_periods(FINANCIAL_RESULTS_START_PERIOD, FINANCIAL_RESULTS_END_PERIOD)a
+FINANCIAL_RESULTS_PERIODS = generate_periods(FINANCIAL_RESULTS_START_PERIOD, FINANCIAL_RESULTS_END_PERIOD)
