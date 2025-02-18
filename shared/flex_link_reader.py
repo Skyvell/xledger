5566eede19597787ae6ca6a08f9b59044a19d3dd
Ted Skyvell <tskyvell@gmail.com>
2025-02-18 10:24:07 +0100

    Clean up flexlink reader class.

--- shared/flex_link_reader.py
+++ shared/flex_link_reader.py
@@ -3,23 +3,24 @@ import io
 from shared.utils.files import convert_xlsx_to_parquet_pandas
 
 class FlexLinkReader:
-    def __init__(self):
-        pass
+    """
+    A utility class for fetching Excel (.xlsx) files from a URL and converting them to Parquet format.
+    """
 
     def read_xlsx_flex_link(self, flex_link: str, column_dtypes: dict, parameter_filters: dict = None) -> io.BytesIO:
         """
-        Fetches an Excel (.xlsx) file from a URL, converts it to Parquet format, and returns the result as an in-memory byte stream.
+        Fetches an Excel file, converts it to Parquet, and returns it as a byte stream.
 
         Args:
-            flex_link (str): The URL of the Excel file to be fetched.
-            column_dtypes (dict): A dictionary specifying the data types of the columns to be included during the conversion to Parquet.
-            parameter_filters (dict, optional): A dictionary of query parameters to append to the URL. If None, no filters are applied.
+            flex_link (str): URL of the Excel file.
+            column_dtypes (dict): Column data types for conversion.
+            parameter_filters (dict, optional): Query parameters for the request.
 
         Returns:
-            io.BytesIO: A byte stream containing the Parquet data converted from the Excel file.
+            io.BytesIO: Parquet data as a byte stream.
 
         Raises:
-            requests.exceptions.HTTPError: Raised when the request to retrieve the Excel file fails.
+            requests.exceptions.HTTPError: If the request fails.
         """
         if parameter_filters:
             response = requests.get(flex_link, params=parameter_filters)
@@ -28,7 +29,6 @@ class FlexLinkReader:
             
         if response.status_code == 200:
             data_bytes_xlsx = io.BytesIO(response.content)
-            data_bytes_parquet = convert_xlsx_to_parquet_pandas(data_bytes_xlsx, column_dtypes)
-            return data_bytes_parquet
+            return convert_xlsx_to_parquet_pandas(data_bytes_xlsx, column_dtypes)
         else:
             raise requests.exceptions.HTTPError(f"Failed to retrieve data from the flex_link. HTTP Status Code: {response.status_code}")
