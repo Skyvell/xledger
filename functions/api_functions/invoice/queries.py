0ea31b00a9eee8dd82e98c7785fa3a3fbf875465
Ted Skyvell <tskyvell@gmail.com>
2025-03-07 09:46:54 +0100

    String to enum

--- functions/api_functions/invoice/queries.py
+++ functions/api_functions/invoice/queries.py
@@ -3,7 +3,7 @@ from gql import gql
 
 GET_INVOICE_PDF = gql("""
 query get_invoice_pdf($invoiceNumber: String!) {
-  arTransactions(first: 1, ownerSet: "MINE",  filter: { invoiceNumber: $invoiceNumber }) {
+  arTransactions(first: 1, ownerSet: MINE,  filter: { invoiceNumber: $invoiceNumber }) {
     edges {
       node {
         invoiceFile {
