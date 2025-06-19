ea95370692d2458767d817cc8c8c3daa8f770d0e
Ted Skyvell <tskyvell@gmail.com>
2025-06-19 15:31:53 +0200

    Now display date in filname and deletes files.

--- functions/api_functions/preliminary_timesheets/get_preliminary_timesheets.py
+++ functions/api_functions/preliminary_timesheets/get_preliminary_timesheets.py
@@ -6,7 +6,7 @@ from shared.data_lake_writer import DataLakeWriter
 from shared.item_fetcher import ItemFetcher
 from shared.gql_client import GraphQLClient
 from shared.environment_config import EnvironmentConfig
-from shared.utils.time import get_first_day_of_previous_month
+from shared.utils.time import get_first_day_of_previous_month, get_current_time_for_filename
 from shared.utils.files import convert_dicts_to_parquet_pandas
 from shared.utils.data_transformation import flatten_list_of_dicts
 
@@ -40,8 +40,7 @@ def get_preliminary_timesheets(myTimer: func.TimerRequest) -> None:
     if not items.has_items():
         return
     
-    # Transform items to the required format adn write to storage account.
+    # Transform items to the required format an write to storage account.
     items_transformed = convert_dicts_to_parquet_pandas(flatten_list_of_dicts(items.get_items()), COLUMN_DTYPES)
     data_lake_writer.delete_all_files_in_directory()
-    data_lake_writer.write_data(f"{NAME}.parquet", items_transformed)
-    
+    data_lake_writer.write_data(f"{get_current_time_for_filename()}-{NAME}.parquet", items_transformed)
