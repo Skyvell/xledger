7e2a5e270e94457ded5b54840a3f988ffefbec48
Ted Skyvell <tskyvell@gmail.com>
2025-07-24 11:09:22 +0200

    Remove old code.

--- shared/configuration_manager.py
+++ shared/configuration_manager.py
@@ -5,42 +5,6 @@ from azure.core.exceptions import ResourceNotFoundError, AzureError
 from azure.identity import DefaultAzureCredential
 
 
-class ConfigurationManager:
-    def __init__(self, connection_string: str, prefix: str = ''):
-        self.client = AzureAppConfigurationClient.from_connection_string(connection_string)
-        self.settings = {}
-        self.prefix = prefix
-
-    def load(self):
-        try:
-            fetched_kv = self.client.list_configuration_settings()
-            self.settings = {item.key[len(self.prefix):]: item.value
-                             for item in fetched_kv if item.key.startswith(self.prefix)}
-            logging.info("Configuration loaded successfully.")
-        except Exception as e:
-            logging.error(f"Failed to load configuration: {e}")
-
-    def save(self):
-        try:
-            for key, value in self.settings.items():
-                self.client.set_configuration_setting(ConfigurationSetting(key=self.prefix + key, value=value))
-            logging.info("Configuration saved successfully.")
-        except Exception as e:
-            logging.error(f"Failed to save configuration: {e}")
-
-    def __getattr__(self, name):
-        if name in ['client', 'settings', 'prefix']:
-            return super().__getattr__(name)
-        return self.settings.get(name, None)
-
-    def __setattr__(self, name, value):
-        if name in ['client', 'settings', 'prefix']:
-            super().__setattr__(name, value)
-        else:
-            self.settings[name] = value
-            logging.info(f"Setting updated - {name}: {value}")
-
-
 class SynchronizerStateManager:
     """
     Manages the synchronizer state using Azure App Configuration.
