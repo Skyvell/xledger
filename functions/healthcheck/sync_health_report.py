19ef8357ede34000bdaa7fc4e941a0d6c1f36d76
Ted Skyvell <tskyvell@gmail.com>
2025-07-29 16:03:18 +0200

    bugfix

--- functions/healthcheck/sync_health_report.py
+++ functions/healthcheck/sync_health_report.py
@@ -11,11 +11,11 @@ from shared.logging import configure_application_logger
 from functions.healthcheck.config import SYNC_ALERT_THRESHOLDS_BY_FUNCTION
 from functions.healthcheck.report import SyncFunctionHealthReport
 
-NAME = "sync_health_check"
+NAME = "sync_health_report"
 SCHEDULE = os.getenv("SYNC_HEALTH_CHECK_SCHEDULE")
 
 logger = configure_application_logger(
-    name = "sync_health_check",
+    name = "sync_health_report",
     log_level = logging.INFO,
     connection_string = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING", "")
 )
@@ -36,12 +36,14 @@ def sync_health_check(myTimer: func.TimerRequest) -> None:
         )
 
         last_updated_iso = state_manager.delta_cursor_updated_at
+        logger.info("Before parsing.")
         last_updated_at = isoparse(last_updated_iso) if last_updated_iso else None
+        logger.info("Afterparsing.")
 
         report = SyncFunctionHealthReport(
-            function_name=function_name,
-            time_until_warning=threshold,
-            last_updated_at=last_updated_at,
-            logger=logger
+            function_name,
+            threshold,
+            last_updated_at,
+            logger
         )
         report.emit_telemetry(datetime.now(datetime.now(timezone.utc)))
